6f716412553dc6e3b755c216596cb0f4b1f42e39
Roman <roma3928@mai.ru>
2023-02-27 01:39:54 +0300

    add footer and adaptive

--- js/main.js
+++ js/main.js
@@ -11,47 +11,65 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // GALLERY
+  lightGallery(document.getElementById("animated-thumbnails-gallery-left"), {});
   lightGallery(document.getElementById("animated-thumbnails-gallery"), {});
 
-// show contact-modal
-const contactBtn = document.getElementById("contact-btn");
-const contactModal = document.getElementById("contact-modal")
+  // show contact-modal
+  const contactBtn = document.getElementById("contact-btn");
+  const contactModal = document.getElementById("contact-modal")
 
 
-function showContactModal() {
-  if (contactModal.classList.contains("contact-modal_active")) {
-    contactModal.classList.remove("contact-modal_active");
+  function showContactModal() {
+    if (contactModal.classList.contains("contact-modal_active")) {
+      contactModal.classList.remove("contact-modal_active");
+    }
+    else {
+      contactModal.classList.add("contact-modal_active");
+    }
   }
-  else {
-    contactModal.classList.add("contact-modal_active");
+
+  contactBtn.addEventListener('click', showContactModal);
+
+   // show nav-modal
+   const navBtn = document.getElementById("nav-btn");
+   const navModal = document.getElementById("nav-modal")
+ 
+ 
+   function showNavModal() {
+     if (navModal.classList.contains("nav-modal_active")) {
+      navModal.classList.remove("nav-modal_active");
+     }
+     else {
+      navModal.classList.add("nav-modal_active");
+     }
+   }
+ 
+   navBtn.addEventListener('click', showNavModal);
+ 
+
+  // show acide
+
+  const asideFirstBtn = document.getElementById("aside-btn-first");
+  const asideSecondBtn = document.getElementById("aside-btn-second");
+  const asideAccordion = document.getElementById("aside-accordion");
+  const asideRightBlock = document.getElementById("aside-box-right");
+
+
+  if (asideFirstBtn) {
+    asideFirstBtn.addEventListener('click', () => {
+      asideFirstBtn.classList.add("aside__tumblers-btn_active")
+      asideAccordion.classList.add("aside_active")
+      asideSecondBtn.classList.remove("aside__tumblers-btn_active")
+      asideRightBlock.classList.remove("aside_active")
+    })
   }
-}
 
-contactBtn.addEventListener('click', showContactModal);
-
-// show acide
-
-const asideFirstBtn = document.getElementById("aside-btn-first");
-const asideSecondBtn = document.getElementById("aside-btn-second");
-const asideAccordion = document.getElementById("aside-accordion");
-const asideRightBlock = document.getElementById("aside-box-right");
-
-
-if (asideFirstBtn) {
-  asideFirstBtn.addEventListener('click', () => {
-    asideFirstBtn.classList.add("aside__tumblers-btn_active")
-    asideAccordion.classList.add("aside_active")
-    asideSecondBtn.classList.remove("aside__tumblers-btn_active")
-    asideRightBlock.classList.remove("aside_active")
-  })
-}
-
-if (asideSecondBtn) {
-  asideSecondBtn.addEventListener('click', () => {
-    asideSecondBtn.classList.add("aside__tumblers-btn_active")
-    asideRightBlock.classList.add("aside_active")
-    asideFirstBtn.classList.remove("aside__tumblers-btn_active")
-    asideAccordion.classList.remove("aside_active")
-  })
-}
+  if (asideSecondBtn) {
+    asideSecondBtn.addEventListener('click', () => {
+      asideSecondBtn.classList.add("aside__tumblers-btn_active")
+      asideRightBlock.classList.add("aside_active")
+      asideFirstBtn.classList.remove("aside__tumblers-btn_active")
+      asideAccordion.classList.remove("aside_active")
+    })
+  }
 });
